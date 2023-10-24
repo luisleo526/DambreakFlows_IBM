5f763cbd4d2d7e3faabe5ec2f3ce7fd965697b4e
leo <luisleo52655@gmail.com>
2023-10-24 17:00:33 +0800

    debug

--- new/run_all.sh
+++ new/run_all.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-declare -a cases=(
-    "A 0.408"
-    "B 0.696"
-    "C 0.952"
-    "D 1.194"
-)
+case=$1
+
+if [ $case = "A" ];then
+	u = "0.408"
+elif [ $case = "B" ];then
+	u = "0.696"
+elif [ $case = "C" ];then
+	u = "0.952"
+elif [ $case = "D" ];then
+	u = "1.194"
+fi
 
 declare -a params=(
-    "16 4 0.005"
-    "24 4 0.005"
-    "32 5 0.005"
+    "16 4 0.01"
+    "24 4 0.01"
+    "32 5 0.01"
 )
 
 declare -a methods=(
@@ -18,17 +23,24 @@ declare -a methods=(
     "3 CLSVOF"
 )
 
+rm -rf logs/$case
+mkdir -p logs/$case
 
-for caseArgs in "${cases[@]}"; 
+for paramArgs in "${params[@]}"; 
 do
-    read -a caseInfo <<< "$caseArgs"
-    for paramArgs in "${params[@]}"; 
+    read -a paramInfo <<< "$paramArgs" 
+    for methodArgs in "${methods[@]}";
     do
-    	read -a paramInfo <<< "$paramArgs" 
-    	for methodArgs in "${methods[@]}";
-    	do
-    		read -a methodInfo <<< "$methodArgs"
-    		sed -i -e "4 c Case${caseInfo[0]}_${methodInfo[1]}_${paramInfo[0]}" input/default.txt
-    	done	 
-    done
+        read -a methodInfo <<< "$methodArgs"
+        name="Case${case}_${methodInfo[1]}_${paramInfo[0]}"
+
+        sed -i -e "4 c ${name}" input/default.txt
+        sed -i -e "12 c ${paramInfo[1]}" input/default.txt
+        sed -i -e "14 c ${paramInfo[0]} 10" input/default.xt
+        sed -i -e "26 c ${paramInfo[2]} 0.5" input/default.txt
+
+        nohup ./RUN &> logs/$case/$name&
+        sleep 3
+
+    done     
 done
