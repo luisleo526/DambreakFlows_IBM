0f9ad0712ae30d0183cc5750a256ad35282735d2
Luis <luisleo52655@gmail.com>
2023-10-28 10:00:45 +0800

    debug

--- scripts/to_gif.sh
+++ scripts/to_gif.sh
@@ -4,13 +4,14 @@ directory=$1
 cores=$2
 
 find POVRAY_FILES -maxdepth 1 -type f -name "*.inc" -exec cp {} $directory \;
+cd $directory
 
 for view in side_view top_view
 do
-    cd $directory
     mkdir -p $view
     sed -i -e "33 c camera{${view}}" camera.inc;
     find . -maxdepth 1 -type f -name "*.pov" -exec povray {} +H1080 +W1920 +R3 +A +WT$cores +FN \; -exec sh -c 'mv "${0%.pov}.png" "${1}/${0%.pov}.png"' {} $view \;
     cd $view
     ffmpeg -f image2 -i %d.png output.gif
+    cd ..
 done
