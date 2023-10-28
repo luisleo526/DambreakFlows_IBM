e3cb1436218fd3592bb2b04097f1d155d0d725f9
Luis <luisleo52655@gmail.com>
2023-10-28 10:12:58 +0800

    debug

--- scripts/to_gif.sh
+++ scripts/to_gif.sh
@@ -12,6 +12,6 @@ do
     sed -i -e "33 c camera{${view}}" camera.inc;
     find . -maxdepth 1 -type f -name "*.pov" -exec povray {} +H1080 +W1920 +R3 +A +WT$cores +FN \; -exec sh -c 'mv "${0%.pov}.png" "${1}/${0%.pov}.png"' {} $view \;
     cd $view
-    ffmpeg -f image2 -i %d.png output.gif
+    ffmpeg -f image2 -i %03d.png output.gif
     cd ..
 done
